72707babb257e3dc72095d782ab6731bdd2a2ada
Aaron Dau <aaron.dau@globee.hk>
2020-09-20 11:37:55 +0700

    Removed sample code and added some dependencies

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container, Row } from 'react-bootstrap';
+
+const App = () => {
+    return (
+        <Router>
+            <div className="page">
+                
+            </div>
+        </Router>
+    );
+};
+
+export default App;
