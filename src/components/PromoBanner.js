0d8ffca03a771f6b3b773c398831ed44a02204e0
Aaron Dau <aaron.dau@globee.hk>
2020-10-06 15:47:09 +0700

    PromoBanner component UI completed

--- src/components/PromoBanner.js
+++ src/components/PromoBanner.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { sampleImageURLs } from '../static/resources';
+import Carousel from 'react-elastic-carousel';
 
 const PromoBanner = () => {
     return (
         <div className="promo-banner">
-            <img src={sampleImageURLs.BANNER} alt="Promo Banner" title="Promo Banner" />
+            <Carousel
+                itemsToShow={1}
+                showArrows={false}
+                pagination={false}
+                enableAutoPlay={true}
+                autoPlaySpeed={5000}
+            >
+                <div className="promo-image">
+                    <img src={sampleImageURLs.BANNER} alt="Promo Banner" title="Promo Banner" />
+                </div>
+                <div className="promo-image">
+                    <img src={sampleImageURLs.BANNER} alt="Promo Banner" title="Promo Banner" />
+                </div>
+            </Carousel>
         </div>
     );
 };
