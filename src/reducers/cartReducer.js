59407e87af5c28a9469c7d19643ebed9d2784d04
Aaron Dau <aaron.dau@globee.hk>
2020-10-22 14:10:12 +0700

    Bug fix for cartReducer.js

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,4 +1,4 @@
-import { cart as initialCart } from '../json';
+import { cart as initialCart } from '../store/initialState';
 import {
     ADD_CART_ITEM,
     REMOVE_CART_ITEM
@@ -6,32 +6,31 @@ import {
 
 export default (previousState = initialCart, action) => {
     const newState = { ...previousState };
-    let { amount, items } = newState;
     let existingItem;
 
     switch (action.type) {
         case ADD_CART_ITEM:
-            existingItem = items.find(item => item.product.id === action.product.id);
+            existingItem = newState.items.find(item => item.product.id === action.product.id);
             if (existingItem) {
                 existingItem.quantity += 1;
             } else {
-                items.push({
+                newState.items.push({
                     quantity: 1,
                     product: action.product
                 });
             }
-            amount += 1;
+            newState.amount += 1;
             return newState;
 
         case REMOVE_CART_ITEM:
-            existingItem = items.find(item => item.product.id === action.product.id);
+            existingItem = newState.items.find(item => item.product.id === action.product.id);
             if (existingItem) {
                 if (existingItem.quantity > 1) {
                     existingItem.quantity -= 1;
                 } else {
-                    items = items.filter(item => item.product.id !== existingItem.product.id);
+                    newState.items = newState.items.filter(item => item.product.id !== existingItem.product.id);
                 }
-                amount -= 1;
+                newState.amount -= 1;
                 return newState;
             }
             return previousState;
