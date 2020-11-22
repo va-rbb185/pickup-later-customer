4d735193bffebbbd84c8c3aaece806ec84507b49
Aaron Dau <aaron.dau@globee.hk>
2020-11-22 08:06:30 +0700

    Init OngoingOrder page

--- src/components/OngoingOrder.js
+++ src/components/OngoingOrder.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'semantic-ui-react';
 import { showCartButton, hideCartButton } from '../actions';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 import PageHeader from './PageHeader';
 
 class OngoingOrder extends React.Component {
@@ -17,13 +20,27 @@ class OngoingOrder extends React.Component {
             // Remember to check `ongoingOrder.orderConfirmation.error` for failed order
             return (
                 <div className="order-details">
-                    <h1>Trang theo dõi đơn hàng đang thực hiện</h1>
+                    <h3>Trang theo dõi đơn hàng đang thực hiện.</h3>
                 </div>
             );
         }
 
         return (
-            <h1>Bạn chưa có đơn hàng đang thực hiện. Hãy đặt đơn hàng một đơn hàng mới đi nào!</h1>
+            <div className="no-order-yet">
+                <div className="icon">
+                    <FontAwesomeIcon icon={faCartPlus} size="4x" />
+                </div>
+                <div className="no-order-yet-msg">
+                    <h3>Bạn chưa có đơn hàng đang thực hiện. Hãy đặt đơn hàng một đơn hàng mới!</h3>
+                </div>
+                <div className="back-to-homepage">
+                    <Button
+                        color="green"
+                        content="Về trang chủ"
+                        onClick={() => this.props.history.replace('/')}
+                    />
+                </div>
+            </div>
         );
     }
 
@@ -39,11 +56,11 @@ class OngoingOrder extends React.Component {
 
 const mapStateToProps = ({ ongoingOrder }) => ({ ongoingOrder });
 
-const actions = {
+const mapDispatchToProps = {
     showCartButton,
     hideCartButton
 };
 
-const ConnectedOngoingOrder = connect(mapStateToProps, actions)(OngoingOrder);
+const ConnectedOngoingOrder = connect(mapStateToProps, mapDispatchToProps)(OngoingOrder);
 
 export default ConnectedOngoingOrder;
